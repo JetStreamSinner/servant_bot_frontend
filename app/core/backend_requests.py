77e1ecdd43b6011de908b70a4bdcc9368fbcd3e5
Jetstream <bigalex934@gmail.com>
2022-08-24 20:49:55 +0300

    Fix comment

--- app/core/backend_requests.py
+++ app/core/backend_requests.py
@@ -4,7 +4,7 @@ import requests
 
 
 def get_services() -> Dict[str, Any]:
-    #TODO Use reverse proxy
+    # TODO Use reverse proxy
     backend_url = os.getenv("backend")
     service_list_router = "/services_list"
     target_url = backend_url + service_list_router
