817bd182f399012423c2923992c48ff2918dbd5f
Jetstream <bigalex934@gmail.com>
2022-08-28 22:48:17 +0300

    Implement calling callback on choosing required service in list

--- app/core/bot_service.py
+++ app/core/bot_service.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any
 from aiogram import Bot, Dispatcher
+from commands_handlers import HandlerType
 
 
 async def start_service(configuration: Dict[str, Any]):
@@ -9,7 +10,11 @@ async def start_service(configuration: Dict[str, Any]):
     commands_handler_bind = configuration["handlers"]
     dispatcher = Dispatcher(bot=bot)
     for binding in commands_handler_bind:
-        dispatcher.register_message_handler(binding["handler"], commands=binding["commands"])
+        match binding["handler_type"]:
+            case HandlerType.MessageHandler:
+                dispatcher.register_message_handler(binding["handler"], commands=binding["commands"])
+            case HandlerType.CallbackQueryHandler:
+                dispatcher.register_callback_query_handler(callback=binding["handler"])
 
     try:
         await dispatcher.start_polling()
--- app/core/commands_handlers.py
+++ app/core/commands_handlers.py
@@ -1,6 +1,12 @@
+import enum
 from aiogram import types
 
 from keyboard_markups import create_main_markup, services_list_markup
+
+
+class HandlerType(enum.Enum):
+    MessageHandler = 0
+    CallbackQueryHandler = 1
 
 
 async def root_handler(message: types.Message):
@@ -17,19 +23,31 @@ async def services_list_handler(message: types.Message):
     await message.answer(text="Service list handler", reply_markup=markup)
 
 
+async def service_handler(query: types.CallbackQuery):
+    await query.answer("Service id is {0}".format(query.data))
+
+
 def get_commands_handler_bind():
     binding = [
         {
             "commands": ["start"],
-            "handler": root_handler
+            "handler": root_handler,
+            "handler_type": HandlerType.MessageHandler
         },
         {
             "commands": ["about"],
-            "handler": about_handler
+            "handler": about_handler,
+            "handler_type": HandlerType.MessageHandler
         },
         {
             "commands": ["services"],
-            "handler": services_list_handler
+            "handler": services_list_handler,
+            "handler_type": HandlerType.MessageHandler
+        },
+        {
+            "commands": [],
+            "handler": service_handler,
+            "handler_type": HandlerType.CallbackQueryHandler
         }
     ]
     return binding
