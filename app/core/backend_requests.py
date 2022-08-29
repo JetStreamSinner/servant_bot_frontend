a63bfdc8aca3a67e947eb8d148f5ad439a9910b9
Jetstream <bigalex934@gmail.com>
2022-08-29 20:40:20 +0300

    Change requests functions return value

--- app/core/backend_requests.py
+++ app/core/backend_requests.py
@@ -3,7 +3,7 @@ from typing import Dict, Any, List
 import requests
 
 
-def get_request(router: str) -> List[Dict[str, Any]]:
+def get_request(router: str) -> Any:
     # TODO Use reverse proxy
     backend_url = os.getenv("backend")
     target_url = backend_url + router
@@ -11,11 +11,11 @@ def get_request(router: str) -> List[Dict[str, Any]]:
     return response.json()
 
 
-def get_services() -> List[Dict[str, Any]]:
+def get_services() -> Any:
     service_list_router = "/services_list"
     return get_request(router=service_list_router)
 
 
-def get_service_info(service_id: int) -> List[Dict[str, Any]]:
+def get_service_info(service_id: int) -> Any:
     service_info_router = "/service_info/{0}".format(service_id)
     return get_request(router=service_info_router)
