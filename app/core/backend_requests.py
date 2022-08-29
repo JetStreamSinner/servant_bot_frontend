d1e8c7b8dc5716cc4b2ca51b052bb692bebcb4c2
Jetstream <bigalex934@gmail.com>
2022-08-29 15:45:57 +0300

    Implement request for receiving service information

--- app/core/backend_requests.py
+++ app/core/backend_requests.py
@@ -3,10 +3,19 @@ from typing import Dict, Any, List
 import requests
 
 
-def get_services() -> List[Dict[str, Any]]:
+def get_request(router: str) -> List[Dict[str, Any]]:
     # TODO Use reverse proxy
     backend_url = os.getenv("backend")
+    target_url = backend_url + router
+    response = requests.get(target_url)
+    return response.json()
+
+
+def get_services() -> List[Dict[str, Any]]:
     service_list_router = "/services_list"
-    target_url = backend_url + service_list_router
-    services_list_request = requests.get(target_url)
-    return services_list_request.json()
+    return get_request(router=service_list_router)
+
+
+def get_service_info(service_id: int) -> List[Dict[str, Any]]:
+    service_info_router = "/service_info/{0}".format(service_id)
+    return get_request(router=service_info_router)
